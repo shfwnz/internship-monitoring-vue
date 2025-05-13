a72a7a8f666eb60f38823ca147a6543f69d2d2e0
shfwnz <ilhamdzaky2007@gmail.com>
2025-05-13 22:35:13 +0700

    fix(navbar): improve navbar

--- src/components/layouts/AppLayout.vue
+++ src/components/layouts/AppLayout.vue
@@ -4,7 +4,7 @@ import Navbar from './Navbar.vue';
 
 <template>
   <div class="m-4 flex flex-col space-y-2">
-    <Navbar />
+    <Navbar class="sticky top-0 z-50 shadow-md" />
     <main>
       <router-view />
     </main>
--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -1,15 +1,40 @@
 <script setup>
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { ref } from 'vue';
+import { useMediaQuery } from '@vueuse/core';
+import { Moon } from 'lucide-vue-next';
+
+// components
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from '@/components/ui/navigation-menu';
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import { Switch } from '@/components/ui/switch';
+
+const isDesktop = useMediaQuery('(min-width: 768px)');
+const isDrawerOpen = ref(false);
+
+const openDrawer = () => {
+  isDrawerOpen.value = true;
+};
+const closeDrawer = () => {
+  isDrawerOpen.value = false;
+};
 
 const links = [
   {
@@ -21,57 +46,123 @@ const links = [
     path: '/app/industry',
   },
 ];
-
-const profileMenu = [
-  {
-    name: 'Profile',
-    path: '/app/profile',
-  },
-];
 </script>
 
 <template>
-  <nav class="flex py-3 px-4 bg-white rounded-2xl">
-    <div class="flex items-center justify-start">Logo</div>
-    <ul class="flex flex-1 gap-4 items-center justify-center">
-      <li v-for="link in links" :key="link.path">
-        <router-link
-          :to="link.path"
-          class="px-3 py-2 rounded-2xl transition-all duration-200 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
-          active-class="bg-amber-400 text-gray-800 font-medium"
-        >
-          {{ link.name }}
-        </router-link>
-      </li>
-    </ul>
-    <div class="flex items-center justify-end gap-3">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger> User</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul class="gap-3 p-0.5">
-                <li v-for="link in profileMenu" :key="link.path">
-                  <NavigationMenuLink as-child>
-                    <a
-                      :href="link.path"
-                      class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                    >
-                      <div class="text-sm font-medium leading-none">
-                        {{ link.name }}
+  <div class="container mx-auto bg-white rounded-2xl">
+    <nav class="flex py-2 px-4">
+      <div class="flex items-center justify-start">SimPKL</div>
+      <ul class="flex flex-1 gap-4 items-center justify-center">
+        <li v-for="link in links" :key="link.path">
+          <router-link
+            :to="link.path"
+            class="px-3 py-2 rounded-2xl transition-all duration-200 text-gray-600 hover:text-gray-800 hover:bg-gray-100"
+            active-class="bg-amber-400 text-gray-800 font-medium"
+          >
+            {{ link.name }}
+          </router-link>
+        </li>
+      </ul>
+      <div class="flex items-center justify-end gap-3">
+        <!-- DESKTOP (DropdownMenu) -->
+        <DropdownMenu v-if="isDesktop">
+          <DropdownMenuTrigger class="flex items-center space-x-2">
+            <span>User</span>
+            <Avatar>
+              <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <router-link to="/app/profile">
+              <DropdownMenuItem class="flex items-center gap-2">
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/unovue.png"
+                    alt="@unovue"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <div>
+                  <p class="font-medium leading-none">Unovue</p>
+                  <p class="text-xs leading-none text-muted-foreground">
+                    @unovue
+                  </p>
+                </div>
+              </DropdownMenuItem>
+            </router-link>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <div class="flex items-center gap-2">
+                <Moon :size="20" />
+                <p class="font-medium leading-none">Dark Mode</p>
+              </div>
+              <Switch :model-value="isDark" @update:model-value="toggleTheme">
+                <template #thumb>
+                  <Icon v-if="isDark" icon="lucide:moon" class="size-3" />
+                  <Icon v-else icon="lucide:sun" class="size-3" />
+                </template>
+              </Switch>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <!-- MOBILE (Drawer) -->
+        <div v-else>
+          <button class="flex items-center space-x-2" @click="openDrawer">
+            <span>User</span>
+            <Avatar>
+              <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </button>
+
+          <Drawer v-model:open="isDrawerOpen">
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle class="py-2 px-4 rounded-md hover:bg-gray-200">
+                  <router-link to="/app/profile">
+                    <div class="flex items-center gap-2">
+                      <Avatar>
+                        <AvatarImage
+                          src="https://github.com/unovue.png"
+                          alt="@unovue"
+                        />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <p class="font-medium leading-none">Unovue</p>
+                        <p class="text-xs leading-none text-muted-foreground">
+                          @unovue
+                        </p>
                       </div>
-                    </a>
-                  </NavigationMenuLink>
-                </li>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-      <Avatar>
-        <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-    </div>
-  </nav>
+                    </div>
+                  </router-link>
+                </DrawerTitle>
+              </DrawerHeader>
+
+              <div
+                class="px-4 pt-2 pb-6 hover:bg-gray-200 rounded-md mx-4 flex items-center gap-2"
+              >
+                <div class="flex items-center gap-2">
+                  <Moon :size="20" />
+                  <p class="font-medium leading-none">Dark Mode</p>
+                </div>
+                <Switch :model-value="isDark" @update:model-value="toggleTheme">
+                  <template #thumb>
+                    <Icon v-if="isDark" icon="lucide:moon" class="size-3" />
+                    <Icon v-else icon="lucide:sun" class="size-3" />
+                  </template>
+                </Switch>
+              </div>
+
+              <DrawerFooter class="pt-4 border-t">
+                <Button variant="outline">Logout</Button>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
