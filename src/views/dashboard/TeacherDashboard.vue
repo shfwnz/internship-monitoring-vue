f72336cea8fb02a142bd759cb32a250056a8e8d6
shfwnz <ilhamdzaky2007@gmail.com>
2025-05-21 22:16:04 +0700

    fix(teacher): teacher dashboard

--- src/views/dashboard/TeacherDashboard.vue
+++ src/views/dashboard/TeacherDashboard.vue
@@ -1,3 +1,29 @@
+<script setup>
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+</script>
+
 <template>
-  <RoleGuard :allowed-roles="['teacher']">Apalah</RoleGuard>
+  <RoleGuard :allowed-roles="['teacher']">
+    <div class="container mx-auto py-2 flex flex-col min-h-screen">
+      <Card>
+        <CardHeader>
+          <CardTitle class="text-xl capitalize"
+            >Student Internship monitoring</CardTitle
+          >
+          <CardDescription class="lowercase"
+            >All student internship information</CardDescription
+          >
+        </CardHeader>
+        <CardContent> Card Content </CardContent>
+        <CardFooter> Card Footer </CardFooter>
+      </Card>
+    </div>
+  </RoleGuard>
 </template>
